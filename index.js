984cbfb0c4fb09c7b827b8cdc1b54c954eab92a0
eri <eri0.sagittarius@gmail.com>
2020-12-04 11:53:49 -0800

    removed extra spacing

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // import the line-reader here
 var lineReader = require('line-reader');
-console.log(lineReader);
 // read the fruits.txt file using the function 
 lineReader.eachLine('./fruits.txt', function (line) {
     console.log(line);
-    
 });
 
